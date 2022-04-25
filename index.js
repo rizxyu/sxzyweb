c4d9d309df99b4d34dce5eb1e05d393f13725425
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-25 09:29:58 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    cors = require('cors'),
+    secure = require('ssl-express-www');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+var { color } = require('./lib/color.js')
+
+var app = express()
+app.enable('trust proxy');
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(express.static("assets"))
+
+app.listen(PORT, () => {
+    console.log(color("Server running on port " + PORT,'green'))
+})
+
+module.exports = app
